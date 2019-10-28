6c3adf99436dd342d9f3954ccdecee1ec75178fc
Durga Prasad Dwarapureddi <durgaprasad@exzatechconsulting.com>
2019-10-28 14:46:51 +0530

    After completing visitor post details and image also

--- app/src/main/java/com/example/retrofitpost/ApiInterface.java
+++ app/src/main/java/com/example/retrofitpost/ApiInterface.java
@@ -6,13 +6,11 @@ import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
-import retrofit2.http.PartMap;
 
 
 public interface ApiInterface {
@@ -23,9 +21,7 @@ public interface ApiInterface {
     Call<Object> uploadImagePost(
                 @Header("Authorization") String Authorization,
                 @Part MultipartBody.Part file
-//                @Body JsonObject fields
             );
-
     @Headers({"Content-Type: application/json;charset=Utf-8"})
     @POST("visitors")
     Call<Object> createPost( @Header("Authorization") String Authorization, @Body JsonObject fields);

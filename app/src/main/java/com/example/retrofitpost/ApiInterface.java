2d10cef6b6b55de7ae6196e23cb1d3d08dd08dac
Durga Prasad Dwarapureddi <durgaprasad@exzatechconsulting.com>
2019-10-28 10:37:08 +0530

    After completing visitor post details and added for image(in progress).

--- app/src/main/java/com/example/retrofitpost/ApiInterface.java
+++ app/src/main/java/com/example/retrofitpost/ApiInterface.java
@@ -1,23 +1,33 @@
 package com.example.retrofitpost;
 
 import com.google.gson.JsonObject;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 
 
 public interface ApiInterface {
 
-    @Headers({"Content-Type: application/json;charset=Utf-8"})
+    // @Headers({"Content-Type: application/json;charset=Utf-8"})
+    @Multipart
     @POST("upload")
-    Call<Object> createPostImage( @Header("Authorization") String Authorization, @Body JsonObject img_user);
+    Call<Object> uploadImagePost(
+                @Header("Authorization") String Authorization,
+                @Part MultipartBody.Part file
+//                @Body JsonObject fields
+            );
 
     @Headers({"Content-Type: application/json;charset=Utf-8"})
     @POST("visitors")
     Call<Object> createPost( @Header("Authorization") String Authorization, @Body JsonObject fields);
-
-
 
 }
